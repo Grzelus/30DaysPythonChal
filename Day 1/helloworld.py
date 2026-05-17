b38a155a0f1f498912db3588cac6be3348d8d4d2
Kacper <grzelakdev@gmail.com>
2026-05-18 01:16:33 +0200

    day_1 completed

---
+++ Day 1/helloworld.py
@@ -0,0 +1,20 @@
+#2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#3
+print('Kacper')
+print("Grzelak")
+print('''Poland''')
+print('I am enjoying 30 days of python')
+
+#4
+dane = [10, 9.8, 3.14, 4 -4j, ['Asabeneh', 'Python', 'Finland'], 'Kacper', 'Grzelak', 'Poland']
+
+for item in dane:
+    print(type(item))
---
+++ Day 1/level_3.py
@@ -0,0 +1,10 @@
+import math
+
+#1
+data = [1,2.3,4-5j,'Coding',True,[6,7],(8,9),{10,11},{"Name":"Kacper", "Gender": "Male"}]
+
+for item in data:
+    print(type(item))
+
+#2
+print(math.sqrt(pow(2-10,2)+pow(3-8,2)))
